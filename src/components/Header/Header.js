dcc5c50b8e6c57c6c1153adec567d81f15f794f4
Olgica Zlatkovska <olgicazlatkovska@Olgicas-iMac.local>
2024-03-24 16:05:48 -0400

    add active styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,42 +1,38 @@
-import { NavLink } from 'react-router-dom';
-import  './Header.scss';
+import { NavLink } from "react-router-dom";
+import "./Header.scss";
 
 export default function Header() {
+  const navOptions = [
+    { id: 0, name: "Home", path: "" },
+    { id: 1, name: "Home Valuation", path: "valuation" },
+    { id: 2, name: "Sell", path: "sell" },
+    { id: 3, name: "Buy", path: "buy" },
+    { id: 4, name: "Community Guide", path: "guide" },
+    { id: 5, name: "Blog", path: "blog" },
+    { id: 6, name: "Career Page", path: "career" },
+    { id: 7, name: "About Us", path: "about" },
+    { id: 8, name: "Events", path: "events" },
+  ];
 
-    const navOptions = [
-        {id: 0, name: 'HOME', path: ''}, 
-        {id: 1, name: 'HOME VALUATION', path: 'valuation'}, 
-        {id: 2, name: 'SELL', path: 'sell'},
-        {id: 3, name: 'BUY', path: 'buy'},
-        {id: 4, name: 'COMMUNITY GUIDE', path: 'guide'},
-        {id: 5, name: 'BLOG', path: 'blog'},
-        {id: 6, name: 'CAREER PAGE', path: 'career'},
-        {id: 7, name: 'ABOUT US', path: 'about'},
-        {id: 8, name: 'EVENTS', path: 'events'},
-    ];
-
-    return (
-        <div className="header">
-            <div className='header__logo'>
-                <img src='https://images.squarespace-cdn.com/content/v1/59d1cb52cd39c3d497f3ccd8/0cc4af28-9c27-4a76-a878-bfcdafab75ef/New%2BAVHE_Logo-1.png?format=1500w' alt='logo'/>
-            </div>
-            <nav className='header__nav'>
-                <ul>
-                    {
-                        navOptions.map((item) => (
-                                <li className='header__list-item' key={item.id}>
-                                    <NavLink 
-                                        to={`/${item.path}`}
-                                        activeClassName='active'
-                                    >
-                                        {item.name}
-                                    </NavLink>
-                                </li>
-                            )
-                        )
-                    }
-                </ul>
-            </nav>
-        </div>
-    )
+  return (
+    <div className="header">
+      <div className="header__logo">
+        <img
+          src="https://images.squarespace-cdn.com/content/v1/59d1cb52cd39c3d497f3ccd8/0cc4af28-9c27-4a76-a878-bfcdafab75ef/New%2BAVHE_Logo-1.png?format=1500w"
+          alt="logo"
+        />
+      </div>
+      <div className="header__nav">
+        <ul>
+          {navOptions.map((item) => (
+            <li className="header__list-item" key={item.id}>
+              <NavLink to={`/${item.path}`} activeClassName="active">
+                {item.name}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,29 @@
 import Header from "../../components/Header/Header";
+import videoSource from './../../assets/videos/miami-bridge.mp4'
 
 export default function Home() {
   return (
     <>
       <Header />
-      <h1>Content</h1>
+      <div className="header__video">
+        <video
+          muted
+          loop
+          playsInline
+          role='presentation'
+          aria-hidden='true'
+          autoPlay
+          preload='auto'
+        >
+          <source 
+            src={videoSource}
+            type="video/mp4"
+          />
+        </video>
+      </div>
+      <div>
+        fhgjhdfgjdfnvfhgjhdfgjdfnv
+      </div>
     </>
   );
 }
