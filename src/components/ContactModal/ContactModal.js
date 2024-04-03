cdd787e1dc5abd864af9bfe0200bff47075bd0ad
Olgica Zlatkovska <olgicazlatkovska@Olgicas-iMac.local>
2024-04-02 21:30:06 -0400

    add sending email functionality

--- src/components/ContactModal/ContactModal.js
+++ src/components/ContactModal/ContactModal.js
@@ -1,10 +1,16 @@
 import './ContactModal.scss';
-
 import { IoIosClose } from "react-icons/io";
 import * as Yup from 'yup';
+import emailjs from 'emailjs-com'; // Import EmailJS library
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 export default function ContactModal({ showModal, toggleModal }) {
+
+    const env = {
+        SERVICE_ID: 'service_7sxk4sc',
+        TEMPLATE_ID: 'contact_form',
+        PUBLIC_KEY: 'Whngj3oMgM3tpqLMG'
+    }
 
     const contactFormSchema = Yup.object().shape({
         firstName: Yup.string()
@@ -27,8 +33,46 @@ export default function ContactModal({ showModal, toggleModal }) {
             .required('Required'),
         message: Yup.string()
             .min(10, 'Message is too short')
-            .required('Required')
+            .required('Required'),
+        marketingConsent: Yup.bool()
+            .oneOf([true], 'You must accept the marketing communications to continue.')
     });
+
+    const handleSubmit = (values, { setSubmitting, resetForm }) => {
+        const formParameters = {
+            name: values.firstName + ' ' + values.lastName,
+            email: values.email,
+            phoneNumber: values.phoneNumber,
+            subject: values.subject,
+            message: values.message,
+            marketingConsent: values.marketingConsent
+        }
+
+        // let formData = new FormData();
+        // formData.append('firstName', values.firstName);
+        // formData.append('lastName', values.lastName);
+        // formData.append('email', values.email);
+        // formData.append('phoneNumber', values.phoneNumber);
+        // formData.append('subject', values.subject);
+        // formData.append('message', values.message);
+        // // Convert boolean to a string representation for EmailJS
+        // formData.append('marketingConsent', values.marketingConsent ? 'Yes' : 'No');
+
+        emailjs.send(env.SERVICE_ID, env.TEMPLATE_ID, formParameters, env.PUBLIC_KEY)
+            .then((response) => {
+                //console.log('Email sent successfully!', response);
+                alert('Your message has been sent successfully!');
+                resetForm();
+            })
+            .catch((error) => {
+                //console.error('Error sending email:', error);
+                alert('An error occurred while sending your message. Please try again later.');
+            })
+            .finally(() => {
+                setSubmitting(false);
+                toggleModal();
+            });
+    }
 
     if (!showModal) {
         return null;
@@ -51,41 +95,44 @@ export default function ContactModal({ showModal, toggleModal }) {
                             email: '',
                             phoneNumber: '',
                             subject: '',
-                            message: ''
+                            message: '',
+                            marketingConsent: false
                         }}
                         validationSchema={contactFormSchema}
-                        onSubmit={(values, { setSubmitting, resetForm }) => {
-                            setTimeout(() => {
-                                alert(JSON.stringify(values, null, 2));
-                                resetForm();
-                                setSubmitting(false);
-                            }, 400);
-                        }}
+                        onSubmit={handleSubmit}
                     >
                         <Form>
-                            <label htmlFor='firstName'>First Name</label>
-                            <Field name='firstName' type='text' />
-                            <ErrorMessage name='firstName' component='div' />
+                            <label className='contact-modal__label' htmlFor='firstName'>First Name</label>
+                            <Field name='firstName' id='firstName' type='text' />
+                            <ErrorMessage className='contact-modal__error-message' name='firstName' component='div' />
 
-                            <label htmlFor='lastName'>Last Name</label>
-                            <Field name='lastName' type='text' />
-                            <ErrorMessage name='lastName' component='div' />
+                            <label className='contact-modal__label' htmlFor='lastName'>Last Name</label>
+                            <Field name='lastName' id='lastName' type='text' />
+                            <ErrorMessage className='contact-modal__error-message' name='lastName' component='div' />
 
-                            <label htmlFor='email'>Email</label>
-                            <Field name='email' type='email' />
-                            <ErrorMessage name='email' component='div' />
+                            <label className='contact-modal__label' htmlFor='email'>Email</label>
+                            <Field name='email' id='email' type='email' autoComplete='email' />
+                            <ErrorMessage className='contact-modal__error-message' name='email' component='div' />
 
-                            <label htmlFor='phoneNumber'>Phone Number</label>
-                            <Field name='phoneNumber' type='tel' />
-                            <ErrorMessage name='phoneNumber' component='div' />
+                            <label className='contact-modal__label' htmlFor='phoneNumber'>Phone Number</label>
+                            <Field name='phoneNumber' id='phoneNumber' type='tel' />
+                            <ErrorMessage className='contact-modal__error-message' name='phoneNumber' component='div' />
 
-                            <label htmlFor='subject'>Subject</label>
-                            <Field name='subject' type='text' />
-                            <ErrorMessage name='subject' component='div' />
+                            <label className='contact-modal__label' htmlFor='subject'>Subject</label>
+                            <Field name='subject' id='subject' type='text' />
+                            <ErrorMessage className='contact-modal__error-message' name='subject' component='div' />
 
-                            <label htmlFor='message'>Message</label>
-                            <Field name='message' as='textarea' />
-                            <ErrorMessage name='message' component='div' />
+                            <label className='contact-modal__label' htmlFor='message'>Message</label>
+                            <Field name='message' id='message' as='textarea' />
+                            <ErrorMessage className='contact-modal__error-message' name='message' component='div' />
+
+                            <div className='contact-modal__consent-container'>
+                                <Field name="marketingConsent" type="checkbox" id="marketingConsent" />
+                                <label className='contact-modal__consent-statement' htmlFor="marketingConsent">
+                                    I agree to receive marketing communications via voice call, AI voice call, text message or similar automated means from Keller Williams Realty Miami Lakes. Consent is not a condition of purchase. Msg/data rates may apply. Msg frequency varies. Reply STOP to unsubscribe.
+                                </label>
+                            </div>
+                            <ErrorMessage className='contact-modal__error-message' name="marketingConsent" component="div" />
 
                             <button type="submit">Submit</button>
                         </Form>
