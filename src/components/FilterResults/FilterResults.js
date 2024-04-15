2e4490bb840076e51ca83129e9a55b4172e9501e
Olgica Zlatkovska <olgicazlatkovska@Olgicas-iMac.local>
2024-04-15 13:13:19 -0400

    add pagination component

--- src/components/FilterResults/FilterResults.js
+++ src/components/FilterResults/FilterResults.js
@@ -1,21 +1,42 @@
+import { useState } from 'react';
 import ProperyCard from '../PropertyCard/PropertyCard';
 import './FilterResults.scss';
+import { Pagination } from "antd";
 
 export default function FilterResults({ className, selectedView, data }) {
 
-    console.log(data, selectedView);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [itemsPerPage, setItemsPerPage] = useState(8);
+    
+    const numberPages = data.length;
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentItems = data.slice(indexOfFirstItem, Math.min(indexOfLastItem, data.length));
 
+    const handlePagination = (page, pageSize) => {
+        setCurrentPage(page);
+        setItemsPerPage(pageSize);
+    }
+    
     return (
         <section className={`${className} listings`}>
             <div className='listings__header'>
                 {data.length} (Houses for sale)
             </div>
             <div className={`listings__container listings__container--${selectedView}`}>
-                <ProperyCard property={data[0]} />
-                <ProperyCard property={data[1]} />
-                <ProperyCard property={data[2]} />
-                <ProperyCard property={data[3]} />
-                <ProperyCard property={data[4]} />
+                {
+                    currentItems.map((property, index) => (
+                        <ProperyCard key={index} property={property}/>
+                    ))
+                }
+            </div>
+            <div className='listings__pagination'>
+            <Pagination 
+                current={1} 
+                pageSize={itemsPerPage} 
+                onChange={handlePagination}
+                total={numberPages} 
+            />
             </div>
         </section>
     )
